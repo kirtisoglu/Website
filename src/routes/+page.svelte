83f5df3d5cdb5e63e769dd8cf5c11a527f02c5fc
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2024-11-08 20:22:50 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,8 +58,6 @@
         </h1>
         <p>
           I'm Alaittin - fourth-year Ph.D. student in <a href="https://www.iit.edu/applied-math" target="_blank" rel="noopener noreferrer">applied mathematics</a><Emoji symbol="🧑‍💻" />
-        </p>
-        <p>
           at <a href="https://www.iit.edu" target="_blank" rel="noopener noreferrer">Illinois Institute of Technology</a><Emoji symbol="🏫" />
         </p>
         <p>
