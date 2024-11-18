1a681bda8d4e80df989a9801b6bbd534da24885f
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2024-11-18 13:22:28 -0600

    Update index.svelte

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,8 +1,8 @@
 
 <script context="module">
   export async function preload({ params, query }) {
-    const intro = await this.fetch(`about/intro.json`).then(r => r.json());
-    const content = await this.fetch(`about/content.json`).then(r => r.json());
+    const intro = await this.fetch(`intro.json`).then(r => r.json());
+    const content = await this.fetch(`content.json`).then(r => r.json());
     return { intro, content };
   }
 </script>
