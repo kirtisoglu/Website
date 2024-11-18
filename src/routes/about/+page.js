593bd74fa2ed97a4d3bc980c2098c36ed12d600b
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2024-11-18 17:32:51 -0600

    Update +page.js

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -3,9 +3,9 @@ import { marked } from 'marked';
 export async function load({ fetch }) {
   try {
     const [intro, content, booklist] = await Promise.all([
-      fetch('about/intro.md').then(res => res.text()),
-      fetch('about/bottom-content.md').then(res => res.text()),
-      fetch('about/booklist.md').then(res => res.text())
+      fetch('/about/intro.md').then(res => res.text()),
+      fetch('/about/bottom-content.md').then(res => res.text()),
+      fetch('/about/booklist.md').then(res => res.text())
     ]);
 
     return {
