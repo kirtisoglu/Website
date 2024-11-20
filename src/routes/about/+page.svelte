87a07e74f59f7e48e706251a8425dff0941bc675
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2024-11-19 22:25:56 -0600

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,7 +10,6 @@
 
   onMount(() => {
     console.log('Component mounted');
-    console.log('Intro data:', data.intro); // For debugging
   });
 </script>
 
@@ -18,8 +17,8 @@
   <p>Error: {data.error}</p>
 {:else}
   <div class="container">
-    <span>
-      <div class="intro">
+    <div class="intro-container">
+      <div class="text-content">
         <h1
           in:fly={{ delay: 500, y: 50, duration: 500 }}
           out:fly={{ y: 50, duration: 300 }}>
@@ -28,20 +27,21 @@
             <Emoji symbol={data.intro.emoji} />
           {/if}
         </h1>
-        {#if data.intro.image}
-          <img 
-            src={data.intro.image} 
-            alt="Portrait"
-            in:fade={{ duration: 500, delay: 600 }}
-          />
-        {/if}
         <div
           in:fly={{ delay: 800, y: 50, duration: 500 }}
           out:fly={{ y: 50, duration: 300 }}>
           {@html data.intro.html}
         </div>
       </div>
-    </span>
+      {#if data.intro.image}
+        <img 
+          src={data.intro.image} 
+          alt="Portrait"
+          in:fade={{ duration: 500, delay: 600 }}
+          class="portrait-image"
+        />
+      {/if}
+    </div>
     <div
       in:fade={{ delay: 1100, duration: 500 }}
       out:fly={{ y: 50, duration: 300 }}>
@@ -60,15 +60,24 @@
 <HiddenLinks/>
 
 <style>
-  img {
-    max-width: 100%;
-    height: auto;
-    margin-bottom: 1rem;
-  }
-  .intro {
+  .intro-container {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
+    align-items: flex-start;
+    gap: 2rem;
+  }
+  .text-content {
+    flex: 1;
+  }
+  .portrait-image {
+    max-width: 40%;
+    height: auto;
+  }
+  @media (max-width: 768px) {
+    .intro-container {
+      flex-direction: column;
+    }
+    .portrait-image {
+      max-width: 100%;
+    }
   }
 </style>
