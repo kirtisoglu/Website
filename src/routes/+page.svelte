ba0b9865ba16d39bdcfa758b42912c674fea016f
kirtisoglu <77515254+kirtisoglu@users.noreply.github.com>
2026-03-09 02:44:34 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,8 +86,8 @@
     min-height: calc(100vh - 4rem);
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    padding: 4rem 2rem 3rem;
+    justify-content: flex-start;
+    padding: 6rem 2rem 3rem;
     max-width: 780px;
     margin: 0 auto;
     width: 100%;
